0bbb9425b58a7dcc352ceefa40b16b7c309cf7b7
br-N <brenobl2000@gmail.com>
2022-09-02 21:29:39 -0300

    Estudo de props

--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,10 @@ function List(){
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
-                <Item marca="Ford"/>
+                <Item marca="Ferrari" ano_lancamento={1995}/>
+                <Item marca="Fiat" ano_lancamento={2001}/>
+                <Item marca="Ford" ano_lancamento={2000}/>
+                <Item/>
             </ul>
         </>
     )
